232a3553dce25e0e0f85e485fb40fd10840d0949
Rudransh Srivastava <84043023+RUDRANSH-hub@users.noreply.github.com>
2023-03-28 23:08:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
 import pandas as pd
-from dataprep.eda import create_report
+import pandas_profiling
 import streamlit as st
 
-# def displayPDF(file):
-#     # Opening file from file path
-#     with open(file, "rb") as f:
-#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+from streamlit_pandas_profiling import st_profile_report
 
-#     # Embedding PDF in HTML
-#     pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+pr = df.profile_report()
 
-#     # Displaying File
-#     st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-
-# st.write("Upload your CSV File")
-spectra = st.file_uploader("upload file", type={"csv", "txt"})
-if spectra is not None:
-    spectra_df = pd.read_csv(spectra)
-    # st.write(spectra_df)
-    
-    
-    for i in range(5):
-
-        st.balloons() 
-    st.write("Wait Reporting is opening")
-    report = create_report(spectra_df, title='My Report')
-    report.show_browser()
+st_profile_report(pr)
