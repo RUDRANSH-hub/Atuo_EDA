99e59d01835199a1bd4976036ba364e16e33fd29
Rudransh Srivastava <84043023+RUDRANSH-hub@users.noreply.github.com>
2023-03-28 23:20:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pandas_profiling
 import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
+x=st.file_uploader("Choose a CSV file")
+if(x):
+  
+  df = pd.read_csv(x)
+  pr = df.profile_report()
 
-df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
-pr = df.profile_report()
-
-st_profile_report(pr)
+  st_profile_report(pr)
