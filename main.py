9377c3799de6849446ecfca6793c23cbc370c2f3
Rudransh Srivastava <84043023+RUDRANSH-hub@users.noreply.github.com>
2023-03-28 23:39:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-st.write(Auto Exploratory data analysis (EDA))
+st.write("Auto Exploratory data analysis (EDA)")
 x=st.file_uploader("Upload your CSV or excel file")
 if(x):
   
